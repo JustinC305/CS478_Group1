28ce3289984e5f77eb1dd519dccc459867f6c5f0
CS478SP22 <50128125+escav4545@users.noreply.github.com>
2022-04-09 20:28:09 -0500

    Update CamMovement.cs

--- Scripts/CamMovement.cs
+++ Scripts/CamMovement.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamMovement : MonoBehaviour
-{
-    public GameObject target;
-   
+   public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,3 +15,4 @@ public class CamMovement : MonoBehaviour
         transform.position = new Vector3(target.transform.position.x, transform.position.y, -10);
     }
 }
+
